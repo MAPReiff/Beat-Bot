3e7d13f2a625fcb00dfb85f1347ef058c42d613c
Mitchell Reiff <mapr323@gmail.com>
2019-11-02 17:13:59 -0400

    Bot Code

---
+++ bot.js
@@ -0,0 +1,34 @@
+//invite https://discordapp.com/oauth2/authorize?client_id=640292156338339860&scope=bot&permissions=8
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fs = require('fs');
+const opn = require('opn');
+
+function downloadSong(link){
+    opn(link);
+}
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+var prefix = "!"
+
+client.on('message', msg => {
+
+  if (msg.content === '!ping') {
+    msg.reply('Pong!');
+  }
+  if (msg.content.startsWith('!addsong ') && msg.content.includes('https://beatsaver.com/beatmap/')){
+      var songCode = msg.content.replace('!add https://beatsaver.com/beatmap/', '');
+      downloadSong('beatsaver://' + songCode);
+        return msg.reply('I have downloaded the song with code ' + songCode);
+
+  }
+
+
+});
+
+var token = fs.readFileSync("./token.txt", "utf8");
+
+client.login(token);
