9469ebddc07699afbf5119c031b52c28bc49c799
Mitchell Reiff <mapr323@gmail.com>
2019-11-02 17:16:09 -0400

    Update bot.js
    Removed Link to my instance of the bot

--- bot.js
+++ bot.js
@@ -1,4 +1,3 @@
-//invite https://discordapp.com/oauth2/authorize?client_id=640292156338339860&scope=bot&permissions=8
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
